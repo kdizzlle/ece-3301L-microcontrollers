5f0a9a0fde119f0c474c5d69c3179abd1ee7ca2d
kdizzlle <112121179+kdizzlle@users.noreply.github.com>
2024-10-31 18:25:39 -0700

    lab 10

---
+++ Lab 10/lab_10.X/build/default/production/_ext/828038617/Interrupt.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/_ext/828038617/Interrupt.d  \
+ build/default/production/_ext/828038617/Interrupt.p1:  \
+ ../Lab10_S3_Sample/Interrupt.c  \
+../Lab10_S3_Sample/Interrupt.h 
---
+++ Lab 10/lab_10.X/build/default/production/_ext/828038617/Lab10_S3_sample.p1.d
@@ -0,0 +1,8 @@
+ build/default/production/_ext/828038617/Lab10_S3_sample.d  \
+ build/default/production/_ext/828038617/Lab10_S3_sample.p1:  \
+ ../Lab10_S3_Sample/Lab10_S3_sample.c  \
+../Lab10_S3_Sample/utils.h  \
+../Lab10_S3_Sample/ST7735_TFT.h  \
+../Lab10_S3_Sample/Main_Screen.h  \
+../Lab10_S3_Sample/Interrupt.h  \
+../Lab10_S3_Sample/main.h 
